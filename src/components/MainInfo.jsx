3efbdcac600477211232c671239c507f32a65864
alleksj <alexandrajns.janus@gmail.com>
2022-01-17 19:36:35 +0100

    Start work on menu

--- src/components/MainInfo.jsx
+++ src/components/MainInfo.jsx
@@ -2,17 +2,74 @@ import React from 'react';
 import Button from './Button';
 
 function MainInfo(props) {
+    const renderSwitch = (param) => {
+        switch (param) {
+            case 'Meni':
+                return (
+                    <div className='menu'>
+
+                    </div>
+                )
+            case 'O nama':
+                return (
+                    <div className='split'>
+                        <div>
+                            <h1>Dobrodošli na najlepši splav na zemunskom keju.</h1>
+                            <p>Posetite nas i uživajte u prijatnoj atmosferi našeg lokala kao i u ukusnoj hrani koju pripremamo za Vas.</p>
+                            <Button text='O nama' />
+                        </div>
+                        <div className='food-image'>
+                            <img src={require('../images/home_food.jpg')} alt='Dish'></img>
+                        </div>
+                    </div>
+                );
+            case 'Galerija':
+                return (
+                    <div className='split'>
+                        <div>
+                            <h1>Dobrodošli na najlepši splav na zemunskom keju.</h1>
+                            <p>Posetite nas i uživajte u prijatnoj atmosferi našeg lokala kao i u ukusnoj hrani koju pripremamo za Vas.</p>
+                            <Button text='O nama' />
+                        </div>
+                        <div className='food-image'>
+                            <img src={require('../images/home_food.jpg')} alt='Dish'></img>
+                        </div>
+                    </div>
+                );
+            case 'Privatni događaji':
+                return (
+                    <div className='split'>
+                        <div>
+                            <h1>Dobrodošli na najlepši splav na zemunskom keju.</h1>
+                            <p>Posetite nas i uživajte u prijatnoj atmosferi našeg lokala kao i u ukusnoj hrani koju pripremamo za Vas.</p>
+                            <Button text='O nama' />
+                        </div>
+                        <div className='food-image'>
+                            <img src={require('../images/home_food.jpg')} alt='Dish'></img>
+                        </div>
+                    </div>
+                );
+            default:
+                return (
+                    <div className='split'>
+                        <div>
+                            <h1>Dobrodošli na najlepši splav na zemunskom keju.</h1>
+                            <p>Posetite nas i uživajte u prijatnoj atmosferi našeg lokala kao i u ukusnoj hrani koju pripremamo za Vas.</p>
+                            <Button text='O nama' />
+                        </div>
+                        <div className='food-image'>
+                            <img src={require('../images/home_food.jpg')} alt='Dish'></img>
+                        </div>
+                    </div>
+                );
+        }
+    }
+
     return (
-        <div className='split'>
-            <div>
-                <h1>Dobrodošli na najlepši splav na zemunskom keju.</h1>
-                <p>Posetite nas i uživajte u prijatnoj atmosferi našeg lokala kao i u ukusnoj hrani koju pripremamo za Vas.</p>
-                <Button text='O nama' />
-            </div>
-            <div className='food-image'>
-                <img src={require('../images/home_food.jpg')} alt='Dish'></img>
-            </div>
+        <div>
+            {renderSwitch(props.activeItem)}
         </div>
+
     )
 }
 
--- src/data/menu.js
+++ src/data/menu.js
@@ -1,5 +1,12 @@
 const menu = [
-
+    {
+        category: 'Predjela',
+        food: {
+            name: 'Supa',
+            description: 'Svakodnevno spremana po tradicionalnim receptima',
+            price: '1000'
+        }
+    }
 ];
 
 export default menu;
